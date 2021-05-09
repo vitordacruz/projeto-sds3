629ec9c3d0380a022e1ab1151b50cbbc36fc7ae0
vitor <vitordacruz@gmail.com>
2021-05-09 01:14:37 -0300

    Melhoria na Paginacao

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -5,24 +5,82 @@ type Props = {
     onPageChange: Function;
 }
 
-const Pagination = ( { page, onPageChange } : Props) => {
+const Pagination = ({ page, onPageChange }: Props) => {
+
+    const itensAfter = [];
+
+    const itensBefore = [];
+
+    let beginNumberColumn = page.number - 4;
+    let endNumberColumn = page.number + 5;
+
+    if (endNumberColumn > page.totalPages) {
+        endNumberColumn = page.totalPages;
+    }
+
+    if (beginNumberColumn < 0) {
+
+        endNumberColumn = (beginNumberColumn * -1) + endNumberColumn;
+
+        if (endNumberColumn > page.totalPages) {
+            endNumberColumn = page.totalPages;
+        }
+
+    } else {
+        const test = endNumberColumn - page.number;
+        if (test != 5) {
+            const diferenca = 5 - test;
+            beginNumberColumn = (diferenca * -1) + beginNumberColumn;
+        }
+    }
+
+    if (beginNumberColumn < 0) {
+        beginNumberColumn = 0;
+    }
+
+    for (let i = (page.number + 1); i < endNumberColumn; i++) {
+        itensAfter.push(
+            <li className="page-item" key={i}>
+                <button className="page-link" onClick={() => onPageChange(i)}>{ i +1 }</button>
+            </li>
+        );
+    }
+
+    for (let i = beginNumberColumn; i < page.number; i++) {
+        itensBefore.push(
+            <li className="page-item" key={i}>
+                <button className="page-link" onClick={() => onPageChange(i)}>{ i + 1 }</button>
+            </li>
+        );
+    }
 
     return (
+        <>
+        <span><strong>Total de páginas: </strong>{ page.totalPages }</span>
         <div className="row d-flex justify-content-center">
             <nav>
                 <ul className="pagination">
-                    <li className={`page-item ${page.first ? 'disabled': ''}`}>
+                    <li className={`page-item ${page.first ? 'disabled' : ''}`}>
+                        <button className="page-link" onClick={() => onPageChange(0)}>Primeira</button>
+                    </li>
+                    <li className={`page-item ${page.first ? 'disabled' : ''}`}>
                         <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
                     </li>
+                    { itensBefore }
                     <li className="page-item disabled">
-                        <span className="page-link">{ page.number + 1 }</span>
+                        <span className="page-link">{page.number + 1}</span>
                     </li>
-                    <li className={`page-item ${page.last ? 'disabled': ''}`}>
+                    { itensAfter }
+                    <li className={`page-item ${page.last ? 'disabled' : ''}`}>
                         <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima</button>
+                    </li>
+                    <li className={`page-item ${page.last ? 'disabled' : ''}`}>
+                        <button className="page-link" onClick={() => onPageChange(page.totalPages - 1)}>Última</button>
                     </li>
                 </ul>
             </nav>
         </div>
+        </>
     );
 };
 
